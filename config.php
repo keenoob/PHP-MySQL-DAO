76211c0d47e332b87b99bc6c711b4d236d0de4c2
Bruno Moraes <brnmrs23@gmail.com>
2017-12-05 15:54:44 -0200

    iniciei o projeto, criando as classes base sql.php e config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+spl_autoload_register(function($classname){
+	
+	$filename = "class". DIRECTORY_SEPARATOR .$classname.".php";
+	if(file_exists($filename)){
+		require_once($filename);
+	}});
